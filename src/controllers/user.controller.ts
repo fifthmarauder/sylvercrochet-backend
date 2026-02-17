2283c1ce2602ef2555dd91d0735fef3387671807
fifthmarauder <eeshashahbaz12345@gmail.com>
2026-02-17 19:17:55 +0500

    Chore:added update and delete product controllers

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,4 +1,4 @@
-import {  addANewProduct, createAUser, getAllProducts, getProductStatistics } from "../services/user.service";
+import {  addANewProduct, createAUser, editProduct, getAllProducts, getProductStatistics } from "../services/user.service";
 import { Request, Response } from "express";
 
 // export const getUsers = async (req, res) => {
@@ -62,3 +62,18 @@ export const getProducts =async(req:Request, res:Response)=>{
     })
   }
 }
+
+export const updateProductController = async(req:Request, res:Response)=>{
+  try {
+    const {id} = req.params;
+    const update = await editProduct(id, req.body)
+    res.status(200).json(update)
+
+  } catch (error:any) {
+    console.error("Product not updated", error)
+    res.status(500).json({
+      message:error.message || "Failed to update products"
+    })
+  }
+}
+
